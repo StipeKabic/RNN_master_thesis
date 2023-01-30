8d4854937e73668a1f8909b8b2da6a8538de2ad9
StipeKabic <stipe145@gmail.com>
2023-01-30 15:19:04 +0100

    spectrograms fixed

--- src/models/spectrograms/spectrograms.py
+++ src/models/spectrograms/spectrograms.py
@@ -1,29 +1,46 @@
 from torch import stft, istft, nn
-
+import numpy as np
+import torch
+import einops
 
 class Spectrogram(nn.Module):
-    def __init__(self, n_fft: int):
+    def __init__(self, n_fft: int, batch_size: int):
         super().__init__()
         self.n_fft = n_fft
+        self.batch_size = batch_size
 
     def forward(self, x):
         b, c, t = x.shape
-        x = x.reshape(b*c,t)
+        x = einops.rearrange(x, 'b c t -> (b c) t')
         x = stft(x, n_fft=self.n_fft, hop_length=490)
-        x = x.permute(0,3,1,2)
-        _, _, freq, time = x.shape
-        x = x.reshape(b, -1, freq, time)
+        x = einops.rearrange(x, '(b c1) f t (c2) -> b (c1 c2) f t', b=self.batch_size)
         return x
 
 
 class InverseSpectrogram(nn.Module):
-    def __init__(self, n_fft: int):
+    def __init__(self, n_fft: int, batch_size: int):
         super().__init__()
         self.n_fft = n_fft
+        self.batch_size = batch_size
 
     def forward(self, x):
-        batch, _, freq, time = x.shape
-        x = x.reshape(batch*2, freq, time, 2)
+        x = einops.rearrange(x, 'b (c1 c2) f t -> (b c1) f t c2', b=self.batch_size, c2=2)
         x = istft(x, n_fft=self.n_fft, hop_length=490)
-        x = x.reshape(batch, 2, -1)
+        x = einops.rearrange(x, '(b c) t -> b c t', b=self.batch_size)
         return x
+
+
+def main():
+    n_fft = 1024
+    batch_size = 4
+    x = np.random.rand(batch_size, 2, 44100)
+    x = torch.tensor(x)
+    spec = Spectrogram(n_fft, batch_size=batch_size)
+    ispec = InverseSpectrogram(n_fft, batch_size=batch_size)
+    y = ispec(spec(x))
+
+    print(torch.norm(y - x))
+
+
+if __name__ == "__main__":
+    main()
